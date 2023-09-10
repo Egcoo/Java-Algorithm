9ce2f9223f02c975612d6097d2ca7cae5b302ac3
Egcoo Fan <2957448049@qq.com>
2023-09-10 18:00:04 +0800

    first update the algorithm

---
+++ BlueBridge/src/TutorialClass/class11/tree1207.java
@@ -0,0 +1,22 @@
+package TutorialClass.class11;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+/**
+ * @ author Egcoo
+ * @ date 2023/3/24 - 16:01
+ * <p>
+ * 树里长度最长的一条路径—— 树的直径
+ */
+public class tree1207 {
+    public static void main(String[] args) {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    }
+}
